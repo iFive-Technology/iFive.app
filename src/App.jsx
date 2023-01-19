4aa670b1840234cd047d56d91eab2019bd5a0449
BestieeMu <93823200+BestieeMu@users.noreply.github.com>
2023-01-19 22:58:04 +0000

    FOLDERS SET

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
       <h1 class="text-3xl bg-green-500 font-bold underline">
-    Hello world!
+    COMING SOON!
   </h1>
     </div>
   )
