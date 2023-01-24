3bdd17eb6c29d35e63b72a9ad89250f848cde10c
BestieeMu <chideraigboka7@gmail.com>
2023-01-24 16:18:57 +0100

    little changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,54 +1,136 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar'
-import './Hero.css'
-import enginner from '../Assets/icons/engineer.png'
-import customer from '../Assets/icons/customer.png'
-import imgBcg from '../Assets/images/background.jfif'
+import React, { useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import "./Hero.css";
+import enginner from "../Assets/icons/engineer.png";
+import customer from "../Assets/icons/customer.png";
+import imgBcg from "../Assets/images/background.jfif";
+
+import { Steps } from 'antd';
+import Input from "antd/es/input/Input";
 
 const Hero = () => {
-  return (
-    <>
-    <div className=" w-full Hero" style={{
-backgroundImage: `url(${imgBcg})`,
-backgroundRepeat: 'no-repeat',
-backgroundSize: 'cover',
-backgroundPosition: 'center',
-boxShadow: 'inset 0 0 0 2000px rgba(0, 0, 0, 0.219)',
-    }}>
-        <NavBar />
-<div className='h-full flex items-center'>
-{/* small cont inside the hero section */}
-<div className='bg-white w-5/12 cus-triger ml-40'>
-    {/* small nav  start */}
-          <div className='w-full flex justify-between  items-center h-20 bg-pink-200'>
-<div className='flex justify-center w-full h-full items-center flex-col customer'><img src={customer} alt="" className='w-8'/> <p>Book Repair</p></div>
-<div className='w-full h-full flex justify-center items-center flex-col enginner'><img src={enginner} alt="" className='w-10'/> <p>Join Enginner Team</p></div>
+  const [switchUSer, setSwitchUser] = useState(true);
+  const [brand,setBrand] = useState('');
+  const [model,setModel] = useState('');
+
+  const handleSwitch = () => {
+    setSwitchUser(true);
+  };
+  const Enginner = () => {
+    setSwitchUser(false);
+  };
+
+  const Enginer =  (
+      <>
+        <div className=" h-full w-full flex flex-col items-center ">
+          <div className="h-64 flex flex-col  items-center ">
+            <h1 className="text-5xl font-bold mt-10 w-10/12 ">
+              Get in the Enginner seat and get paid
+            </h1>
+            <p className="text-1xl mt-5">
+              Drive on the platform with the largest network of active riders.
+            </p>
           </div>
-          {/* small nav end */}
+          {/* text wrapper */}
+          <div className="w-10/12 h-32 flex flex-col justify-between">
+            <div>
+             
+              <button className="bg-green-400 text-1xl p-3 cursor-pointer text-white ">
+                Sign up to repair
+              </button>
+            </div>
+            <div>
+              <p className="text-lg underline">Learn more about being our enginner</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  
 
-<div className=' h-full w-full flex flex-col items-center '>
-    <div className='h-72 flex flex-col justify-around items-center '>
-    <h1 className='text-4xl font-bold w-10/12 '>
-Get in the Enginner seat and get paid
-</h1>
-<p className='text-xl'>Drive on the platform with the largest network of active riders.</p>
+
+
+  const Customer = (
+      <>
+        <div className="  w-full flex flex-col items-center ">
+        <div className="text-5xl w-10/12 flex justify-center  font-bold mt-5 text-black">
+            <h1>Order for a repair now</h1>
+        </div>
+
+        <div className="w-10/12 mt-10   ">
+        <Steps
+    direction="vertical"
+    size="small"
+    current={2}
+    items={[
+      {
+          title: 'Phone Brand',
+          description :  (<div className="flex items-center pl-1 w-full bg-blue-100 h-12"><input type='text' placeholder="e.g Apple"
+           className="h-10 w-full outline-none p-1 text-lg bg-transparent" value={brand} onChange={(e) => setBrand(e.target.value)}/></div>),
+      },
+      {
+        title: 'Phone Model',
+        description :  (<div className=" flex items-center pl-1 w-full bg-blue-100 h-12"><input type='text' placeholder="e.g IPhone 12 pro max"
+         className="h-10 w-full outline-none p-1 text-lg bg-transparent" value={model} onChange={(e) => setModel(e.target.value)}/></div>),
+      },
+   
+    ]}
+  />
+        </div>
+    <div className="w-10/12 mt-5">
+      <button className="bg-green-400 p-3 text-white text-1xl w-36">Order now</button>
     </div>
-    {/* text wrapper */}
-<div className='w-10/12 h-40 flex flex-col justify-between'>
-    <div>    <button className='bg-green-400 text-1xl p-3 cursor-pointer text-white rounded-lg'>Sign up to repair</button></div>
-    <div><p className='text-lg'>Learn more about being our enginner</p></div>
-</div>
-
-</div>
 
         </div>
-</div>
-{/*  small cont inside the hero section ends here */}
- 
+      </>
+    );
+  
 
-    </div>
+  console.log(brand, model);
+  return (
+    <>
+      <div
+        className=" w-full Hero"
+        style={{
+          backgroundImage: `url(${imgBcg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.219)",
+        }}
+      >
+        <NavBar />
+
+        <div className="h-full flex items-center">
+          <>
+            {/* small cont inside the hero section */}
+            <div className="bg-white w-4/12 cus-triger ml-40">
+              {/* small nav  start */}
+              <div className="w-full flex justify-between  items-center h-20 bg-pink-200">
+                <div
+                  className="flex justify-center w-full h-full items-center flex-col customer"
+                  onClick={handleSwitch}
+                >
+                  <img src={customer} alt="" className="w-8" />{" "}
+                  <p>Book Repair</p>
+                </div>
+                <div className="w-full h-full flex justify-center items-center flex-col enginner"  onClick={Enginner}>
+                  <img src={enginner} alt="" className="w-8" />{" "}
+                  <p>Join Enginner Team</p>
+                </div>
+              </div>
+              {/* small nav end */}
+       
+              {/* //////////////customer switch////////////////////// */}
+              {switchUSer ? Customer : Enginer }
+              {/* /////////////////////////////////////// */}
+            </div>
+            {/*  small cont inside the hero section ends here */}
+          </>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './NavBar.css'
-import logo from '../Assets/logos/whiteLogo.png'
+import logo from '../Assets/logos/fixlogo.png'
 const NavBar = () => {
   return (
     <>
 <div className='bg-black h-14 text-white flex items-center px-3 justify-between'>
-    <div className='mt-5'><img src={logo} alt="" className='logo' /></div>
+    <div className=''><img src={logo} alt="" className='logo' /></div>
 
 <div className=' w-4/12 mr-20'>
     <ul className='flex justify-around w-full items-center text-1xl '>
-        <li>Service</li>
-        <li>Product</li>
-        <li>About us</li>
+        <li className='cursor-pointer'>Service</li>
+        <li className='cursor-pointer'>Product</li>
+        <li className='cursor-pointer'>About us</li>
 
         <div className='flex gap-5 items-center '>
-        <li>Login</li>
-        <li className='bg-white text-black w-20  flex justify-center p-1 rounded-lg'>Sign up</li>
+        <li className='cursor-pointer'>Login</li>
+        <li className='bg-white text-black w-20  flex justify-center p-2 rounded-lg cursor-pointer'>Sign up</li>
         </div>
 
     </ul>
